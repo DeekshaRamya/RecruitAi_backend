23fff0b0d19a85e2c43bf6cca7d2670050501ec0
Ramya Karthick Kumar <ramyak@systechusa.com>
2026-07-28 10:55:57 +0530

    bug fix

--- app/main.py
+++ app/main.py
@@ -15,10 +15,7 @@ if sys.platform == "win32":
 
 from sqlalchemy import text
 from app.database.database import engine, Base
-<<<<<<< HEAD
 from app.database.models import User, LoginHistory, Assessment, AssessmentAssignment, CandidateActivityLog, EnglishInterview, EnglishInterviewConversation
-=======
->>>>>>> e8d2c07c883324eb0e30a8186652aec01695c151
 from app.api.auth import router as auth_router
 from app.api.users import router as users_router
 from app.api.candidate import router as candidate_router
